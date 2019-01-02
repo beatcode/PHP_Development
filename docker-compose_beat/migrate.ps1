a8e2dc189edcbb079e4df386e1578aa3b10335c5
beatcode <beat.sun@hotmail.com>
2019-01-02 13:03:33 +0100

    Init

---
+++ docker-compose_beat/migrate.ps1
@@ -0,0 +1,3 @@
+docker exec -it docker-compose_beat_apache_1 /bin/bash
+cd ..
+php yii migrate
---
+++ docker-compose_beat/start.ps1
@@ -0,0 +1,4 @@
+cd C:\Users\beatcode\Documents\bleachandsmile\_Development\docker-compose_beat
+docker-compose stop
+docker-compose rm
+docker-compose up
